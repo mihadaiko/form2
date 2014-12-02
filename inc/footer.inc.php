2943d58a538a1531cb0d7f4cdc67728fb0ed7790
mihadaiko <mihadaiko@gmail.com>
2014-12-01 21:39:25 -0500

    Simple Email Form
    Email Form

---
+++ inc/footer.inc.php
@@ -0,0 +1,8 @@
+<footer>
+<h1>Form Demo</h1>
+<nav>
+    <a href="#" title="" class="home-lk"> home</a>
+    <a href="#" title="" class="about-lk"> about</a>
+    <a href="#" title="" class="current contact-lk">Contact</a>
+    </nav>
+</footer>
